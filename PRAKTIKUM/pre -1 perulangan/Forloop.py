154e43eb9fd024e37c752b42d54d62e108d6c379
Sofianhafiz <sopianhafiz2005@gmail.com>
2025-04-14 13:46:41 +0800

    Add files via upload

---
+++ PRAKTIKUM/pre -1 perulangan/Forloop.py
@@ -0,0 +1,40 @@
+# PERULANGAN (LOOP)
+
+# for-loop
+# for kondisi :
+#   aksi 
+
+a = 0 
+a += 1 # a = a + 1 
+print(a)
+a += 1 
+print(a)
+a =+ 1 
+print(a)
+
+angka_list = [0,1,4,8,10]
+for i in angka_list:
+    print(f"i sekarang -> {i}")
+
+
+# perulangan dengan range 
+angka_range = range(5) # diulang 5 kali 
+
+for i in angka_range:
+    print(f"i sekarang -> {i}")
+
+print("ini akhir for2 \n")
+
+angka_range2 = range(1, 10) 
+for i in angka_range:
+    print(f"i sekarang -> {i}")
+
+print("ini akhir for3 \n")
+
+# latihan
+
+angka_string = string= "sopian"
+for i in angka_string:
+    print(f"i sekarang -> {i}")
+
+print("ini akhir for3 \n")
---
+++ PRAKTIKUM/pre -1 perulangan/While_loop.py
@@ -0,0 +1,15 @@
+# while - loop
+
+# while (kondisi = true) :
+#   aksi # bagian dari while
+# aksi # bukan dari while
+
+angka = 0 
+print(f"angka awal -> {angka}")
+
+# > lebih
+# < kurang
+
+while angka < 5:
+    angka += 1
+    print(f"angka sekarang -> {angka}")
